24bec9e0d3f73562dd77d832890a85a45b3e896a
Arnaud Astruc <oxynux@gmail.com>
2017-08-29 03:31:56 +0200

    ex01 ok

--- 01_dockerfiles/ex01/entrypoint.sh
+++ 01_dockerfiles/ex01/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-
-for i in logs/* do 
-	cat <> ${i} 1>&2 &  
-done
-
+cd /opt/teamspeak/
+export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+/opt/teamspeak/ts3server createinifile=1
+/opt/teamspeak/ts3server_minimal_runscript.sh
+cat <> /opt/teamspeak/ts3server.ini 1>&2 &
 exec "$@"
